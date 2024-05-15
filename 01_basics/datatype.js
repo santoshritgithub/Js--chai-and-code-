23f485bdb2ca1b28cd43a5c07050e2c95f6e1f8b
santosh <124672916+santoshritgithub@users.noreply.github.com>
2024-05-15 13:04:39 +0000

    datatype Added

---
+++ 01_basics/datatype.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all JS code as newer version
+let name ="Santosh";
+let age= 21;
+let val;
+console.log(typeof("name"));//string
+console.log(typeof(age));//number (0 to 2^53)
+console.log(typeof(null));// object
+console.log(typeof(val));// undefined
+
+
