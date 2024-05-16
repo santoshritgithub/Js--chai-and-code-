efb95f717b8471723436e048c1a5505e5d9c12f0
santosh <124672916+santoshritgithub@users.noreply.github.com>
2024-05-16 15:32:49 +0000

    date and time

---
+++ 01_basics/date_time.js
@@ -0,0 +1,10 @@
+let mydate= new Date();
+// console.log(mydate);
+// console.log(mydate.toLocaleDateString());
+
+let creatdate= new Date( "01-14-2024");
+console.log(creatdate.toString());
+
+let newdate= new Date();
+console.log(newdate);
+console.log(newdate.getDay());
