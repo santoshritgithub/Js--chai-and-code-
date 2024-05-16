7d36008d4eebb6291c4feba3c5c00f7e37b71d8b
santosh <124672916+santoshritgithub@users.noreply.github.com>
2024-05-16 13:08:41 +0000

    heap

---
+++ 01_basics/heap.js
@@ -0,0 +1,16 @@
+let gmail= "barwalsantosh279@gmail.com";
+let id="Hellobaby";
+let gmail2= gmail;
+gmail2= "nahi dunga";
+console.log(gmail);
+console.log(gmail2);
+
+const obj = {
+     gmail3 :"barwalsantosh279@gmail.com",
+     id2 :"Hellobaby"
+}
+gmail4=obj;
+gmail4.gmail3="ajfadfksjf";
+console.log(obj.gmail3);
+console.log(gmail4.gmail3);
+
