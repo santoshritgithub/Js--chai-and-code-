17e11c2293062a251b839066bf23e054ea185a54
santosh <124672916+santoshritgithub@users.noreply.github.com>
2024-05-16 06:12:46 +0000

    string added

---
+++ 01_basics/string.js
@@ -0,0 +1,16 @@
+let name= "Ashwin";
+let count= 20;
+// console.log(name+ count + "Hero");
+console.log(`My name is ${name} and my number is ${count}`);
+
+const carname= new String('Mercedes')   // other way of declaration
+console.log(carname.indexOf('r'));
+console.log(carname.toUpperCase());
+console.log(carname.bold());
+
+let newstring= "   Hellow   ";
+console.log(newstring);
+console.log(newstring.trim());
+
+let url ="http://www.google  .com";
+console.log(url.replace('  ', '-'));
