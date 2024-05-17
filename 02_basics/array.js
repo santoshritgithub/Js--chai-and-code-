c5c7ceea472e07f238f7453155aa5d57f79f8083
santosh <124672916+santoshritgithub@users.noreply.github.com>
2024-05-17 06:42:42 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,26 @@
+// let arr= [1,2,3,4,5];
+//  arr.push(6);
+//  console.log(arr);
+//  arr.push(7);
+//  console.log(arr);
+//  arr.pop();
+//  console.log(arr);
+//  arr.unshift(9);
+//  console.log(arr);
+//  arr.shift();
+//  console.log(arr);
+
+//  let arr2= arr.join()
+//  console.log(arr2);
+//  console.log(typeof arr2);
+
+
+//slice and splice
+let Array = [2,1,4,6,8];
+let val= Array.slice(1,3);
+console.log(Array);
+console.log(val);
+
+let val2= Array.splice(1,3);
+console.log(Array);
+console.log(val2);
---
+++ 02_basics/array2.js
@@ -0,0 +1,8 @@
+let arr1= new Array("abdul","biky","raman");
+let arr2 = new Array("Topper","coder","xuprustam");
+console.log(arr1 + arr2);
+
+
+let newarray=[1,2,3,[4,5],5,[7,3,[0,8]]];
+console.log(newarray);
+console.log(newarray.flat(Infinity));
