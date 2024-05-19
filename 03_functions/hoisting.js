36202abf7998b28eae9ef48c57b559a01681732c
santosh <124672916+santoshritgithub@users.noreply.github.com>
2024-05-19 03:48:39 +0000

    scope and hoisting

---
+++ 03_functions/hoisting.js
@@ -0,0 +1,11 @@
+
+
+console.log(addone(3));
+function addone(num){
+    return num+1;
+}
+
+const addtwo = function(val){
+    return val+2
+}
+console.log(addtwo(3));
---
+++ 03_functions/scope.js
@@ -0,0 +1,10 @@
+ 
+if(true){
+    let a= 10
+    var b= 20
+    const c=30
+ }
+ b=80;
+//  console.log(a);
+ console.log(b);
+//  console.log(c);
