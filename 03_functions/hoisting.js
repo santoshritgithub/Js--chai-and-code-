6c480ba2d054493b42e8bd8f12c304dd999f62e5
santosh <124672916+santoshritgithub@users.noreply.github.com>
2024-05-19 04:30:23 +0000

    hosting

--- 03_functions/hoisting.js
+++ 03_functions/hoisting.js
@@ -1,11 +1,11 @@
 
 
-console.log(addone(3));
+console.log(addone(3));// execute properly
 function addone(num){
     return num+1;
 }
 
+console.log(addtwo(3)); //Error: Cannot access 'addtwo' before initialization
 const addtwo = function(val){
     return val+2
 }
-console.log(addtwo(3));
